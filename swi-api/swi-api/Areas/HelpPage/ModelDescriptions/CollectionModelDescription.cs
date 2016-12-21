567fd4fd5fd7968ed1d55b531324114d634352c2
Steve Kent <steve_kent@beaerospace.com>
2016-12-21 16:30:58 +0000

    Added asp.net webapi project template

---
+++ swi-api/swi-api/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace swi_api.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ swi-api/swi-api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="swi_api.WebApiApplication" Language="C#" %>
